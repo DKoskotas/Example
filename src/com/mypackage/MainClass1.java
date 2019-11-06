cffdf09b932a61646ec7ebb29912fdaa49874a5f
Dimitris Koskotas <dkoskotas@gmail.com>
2019-11-06 10:02:05 +0200

    added project Example

---
+++ src/com/mypackage/MainClass1.java
@@ -0,0 +1,45 @@
+
+package com.mypackage;
+
+
+public class MainClass1 {
+
+    
+    
+    public static void main(String[] args) {
+        
+//        int array [] = new int[4];
+//        array [1] = 7;
+//        array [2] = 5;
+//        array [3] = 0;
+//        array [0] = 2;
+        int [] temp = new int [5];
+        System.out.println(temp);
+        
+        int [][] array =
+        {
+            {3, 6, 10},
+            {43},
+            {0, -4},
+            {6, 9, 9, 9, 7}
+        };
+        System.out.println(array[3][4]);
+        
+//        int megethos = array2.length;
+//        System.out.println(megethos);
+        
+//        int i;
+//        for(i =0 ; i < array.length; i++){
+//            System.out.println(array[i]);        
+//        }
+//        
+//        i=0;
+//        while(i < array.length){
+//           System.out.println(array[i]); 
+//           i++;
+//        }
+        
+        
+    }
+    
+}
